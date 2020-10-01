5a64546e14a82b082707735686edb96289e87709
Fernando Santander <nandouluna@gmail.com>
2020-10-01 13:23:23 -0500

    Se agrega modulo 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { Component1Component } from './component1/component1.component';
+import { Componente2Component } from './componente2/componente2.component';
+import { Componente3Component } from './componente3/componente3.component';
+import { Componente4Component } from './componente4/componente4.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Component1Component
+    Component1Component,
+    Componente2Component,
+    Componente3Component,
+    Componente4Component
   ],
   imports: [
     BrowserModule
